b2eceb27e06e09e781821c54dfe1f6e39ba2fda4
Your Name <your@example.com>
2024-11-06 03:50:27 +0800

    Remove all files from repository

--- PhotoBlogProject/PhotoBlogServer/blog/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Post
-from .serializers import PostSerializer
-
-class BlogImages(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
